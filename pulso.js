03e26227da707220fc8bab11c036c53538b63437
Auric Vente <mprops@protonmail.com>
2021-10-12 13:03:46 -0600

    Test

--- pulso.js
+++ pulso.js
@@ -1,11 +1,20 @@
 const express = require("express")
 const config = require("./config.json")
+const http = require("http")
+const crypto = require("crypto")
+const exec = require("child_process").exec
 const app = express()
 
 app.use(express.json())
 
 app.post('/update', (req, res) => {
-  console.log(req.body)
+  let sig = "sha1=" + crypto.createHmac('sha1', config.secret).update(chunk.toString()).digest('hex')
+
+  if (req.headers['x-hub-signature'] === sig) {
+    console.log("valid")
+  } else {
+    console.log("not valid")
+  }
 })
 
 app.listen(config.port, () => {
