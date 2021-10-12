ebd3eedd1f75b47b8fe4e5da8f0093e7086974e8
Auric Vente <mprops@protonmail.com>
2021-10-12 13:14:46 -0600

    Test

--- pulso.js
+++ pulso.js
@@ -1,24 +1,44 @@
+const crypto = require("crypto")
 const express = require("express")
 const config = require("./config.json")
-const http = require("http")
-const crypto = require("crypto")
-const exec = require("child_process").exec
+
+const secret = config.secret
+const sigHeaderName = "X-Hub-Signature-256"
+const sigHashAlg = "sha256"
+
 const app = express()
 
-app.use(express.json())
-
-app.post('/update', (req, res) => {
-  req.on('data', function(chunk) {
-    let sig = "sha1=" + crypto.createHmac('sha1', config.secret).update(chunk.toString()).digest('hex')
-  
-    if (req.headers['x-hub-signature'] === sig) {
-      console.log("valid")
-    } else {
-      console.log("not valid")
+app.use(express.json({
+  verify: (req, res, buf, encoding) => {
+    if (buf && buf.length) {
+      req.rawBody = buf.toString(encoding || "utf8")
     }
-  })
+  },
+}))
+
+function verifyPostData(req, res, next) {
+  if (!req.rawBody) {
+    return next("Request body empty")
+  }
+
+  const sig = Buffer.from(req.get(sigHeaderName) || "", "utf8")
+  const hmac = crypto.createHmac(sigHashAlg, secret)
+  const digest = Buffer.from(sigHashAlg + "=" + hmac.update(req.rawBody).digest("hex"), "utf8")
+  
+  if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+    return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+  }
+
+  return next()
+}
+
+app.post("/update", verifyPostData, function (req, res) {
+  res.status(200).send("Request body was signed")
 })
 
-app.listen(config.port, () => {
-  console.info(`Pulso started on port ${config.port}`)
+app.use((err, req, res, next) => {
+  if (err) console.error(err)
+  res.status(403).send("Request body was not signed or verification failed")
 })
+
+app.listen(config.port, () => console.log(`Listening on port ${config.port}`))
